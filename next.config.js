350ee968e457fd48653477e44c64ffb2ca64db2f
Casa-Chirilagua <database@casachirilagua.org>
2023-11-25 22:04:47 -0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
+
 const nextConfig = {
   reactStrictMode: true,
+  sassOptions:{
+    includePath: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
 }
 
 module.exports = nextConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
-import '@/styles/globals.css'
+import "../styles/globals.scss";
+import Navbar from "@/components/Navbar";
+import { Open_Sans} from 'next/font/google';
 
+const openSans = Open_Sans({ subsets: ['latin'], weight: ['400'] });
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const navBarData = [
+    {
+      id: 1,
+      label: "Projects",
+      url: "/projects",
+    },
+    {
+      id: 2, 
+      label: "About",
+      url: "/about",
+    },
+  ];
+  return (
+    <div className={openSans.className}>
+      {" "}
+      <Navbar menuItems={navBarData} />
+      <Component {...pageProps} />
+    </div>
+  );
 }
