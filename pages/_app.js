6a46099b20e5ef5178178e049e1de18f8ba5ea6b
Casa-Chirilagua <database@casachirilagua.org>
2023-12-21 16:09:42 -0500

    Added Speed  insight

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,8 @@ import Navbar from "@/components/Navbar";
 import { Open_Sans } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import Head from "next/head";
-import { Analytics } from '@vercel/analytics/react';
-
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 const openSans = Open_Sans({ subsets: ["latin"], weight: ["400"] });
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -36,9 +36,15 @@ export default function App({ Component, pageProps }) {
         <Navbar menuItems={navBarData} />
         <Component {...pageProps} />
         <ToastContainer />
-        <motion.div className="absolute top-0 left-0 w-screen h-screen bg-black z-50" initial={{scaleY:0}} animate={{scaleY:0}} exit={{scaleY: 1}} transition={{duration:0.75, ease: [0.22,1, 0.36, 1]}}></motion.div>
+        <motion.div
+          className="absolute top-0 left-0 w-screen h-screen bg-black z-50"
+          initial={{ scaleY: 0 }}
+          animate={{ scaleY: 0 }}
+          exit={{ scaleY: 1 }}
+          transition={{ duration: 0.75, ease: [0.22, 1, 0.36, 1] }}
+        ></motion.div>
         <Analytics />
-
+        <SpeedInsights />
       </motion.div>
     </AnimatePresence>
   );
