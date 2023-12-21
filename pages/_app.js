ff1dc636568efd5d57103d3dbd9acd87eb2efa4e
Casa-Chirilagua <database@casachirilagua.org>
2023-12-21 15:44:02 -0500

    Added Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,8 @@ import Navbar from "@/components/Navbar";
 import { Open_Sans } from "next/font/google";
 import { ToastContainer } from "react-toastify";
 import Head from "next/head";
+import { Analytics } from '@vercel/analytics/react';
+
 const openSans = Open_Sans({ subsets: ["latin"], weight: ["400"] });
 export default function App({ Component, pageProps }) {
   const router = useRouter();
@@ -35,6 +37,7 @@ export default function App({ Component, pageProps }) {
         <Component {...pageProps} />
         <ToastContainer />
         <motion.div className="absolute top-0 left-0 w-screen h-screen bg-black z-50" initial={{scaleY:0}} animate={{scaleY:0}} exit={{scaleY: 1}} transition={{duration:0.75, ease: [0.22,1, 0.36, 1]}}></motion.div>
+        <Analytics />
       </motion.div>
     </AnimatePresence>
   );
