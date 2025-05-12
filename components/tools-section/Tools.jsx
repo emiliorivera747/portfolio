b40dbb82ae9ea7c5bb66c313861a1e0718421bb9
Emilio Rivera <emiliorivera747@gmail.com>
2025-05-12 00:02:13 -0700

    Added Optimized cloudinary images

--- components/tools-section/Tools.jsx
+++ components/tools-section/Tools.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Image from 'next/image'
+import { CldImage } from 'next-cloudinary';
 
 /**
  * Displays tools 
@@ -13,12 +13,12 @@ const Tools = ({ toolsData }) => {
     <div className="h-full w-screen flex overflow-auto px-[0.4rem] sm:px-60 pb-10 items-center justify-center gap-10 flex-wrap">
       {toolsData.map((item, i) => (
         <div key={i} className="flex flex-col items-center justify-center gap-4 backdrop-blur-md">
-          <Image
+          <CldImage
             alt={item.name}
+            name={item.name}
             src={item.imageUrl}
             width={100}
             height={100}
-            loading="lazy"
           />
           <div className="flex item-center justify-center" >
             <p className="text-primary-600 text-xs"> {item.name}</p>
