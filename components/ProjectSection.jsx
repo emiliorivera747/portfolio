df74c0bab2f5dedaa49357c204e692cb36283aef
Emilio Rivera <emiliorivera747@gmail.com>
2025-05-13 01:13:19 -0700

    updated Projection Section button

--- components/ProjectSection.jsx
+++ components/ProjectSection.jsx
@@ -78,7 +78,7 @@ function ProjectSection({
             className={`absolute rounded-md border-2 ${buttonBorderColor} bg-transparent ${buttonTextColor} w-60 h-12 font-semibold self-center justify-center text-center p-2 bottom-0 ${buttonBgColor} ${buttonHoverTextColor} mb-6`}
             aria-label={`Learn more about ${title}`}
           >
-            {buttonLabel}
+            {buttonLabel} <span className="text-transparent">.</span>
           </Link>
         </div>
       </motion.div>
