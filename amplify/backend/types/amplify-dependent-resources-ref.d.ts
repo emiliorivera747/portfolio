b2c0a8c713aaebf2173d3ef2a32d4aa6c5f51228
Emilio Rivera <emiliorivera747@gmail.com>
2025-05-13 15:35:52 -0700

    added analytics

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "portfolio": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "auth": {
+    "portfolio": {
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string"
+    }
+  }
+}
