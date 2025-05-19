0e999d00712749745b3703ba1aca54d32a49a2eb
Emilio Rivera <emiliorivera747@gmail.com>
2025-05-18 20:40:03 -0700

    added photos to casa chirilagua section

--- pages/casa-chirilagua/index.jsx
+++ pages/casa-chirilagua/index.jsx
@@ -45,16 +45,16 @@ function Project1() {
     {
       opacity: 0.4,
       imageUrl:
-        "https://res.cloudinary.com/dcss55nem/image/upload/v1714516775/Auth0Login_hbmnkw.png",
-      title: "Login",
+        "https://res.cloudinary.com/dcss55nem/image/upload/v1747625338/Screenshot_2025-05-18_at_8.23.36_PM_sqxg9w.png",
+      title: "Sign In Page",
       paragraph:
         "The front end and back end are protected with Auth0, a third-party authentication service. Auth0 uses AES-256 to encrypt data at rest and is ISO27018 certified, complying with security and privacy guidelines for managing PII.",
     },
     {
       opacity: 0.4,
       imageUrl:
-        "https://res.cloudinary.com/dcss55nem/image/upload/v1714515640/DashboardPage_x2yel3.png",
-      title: "Dashboard Part 1",
+        "https://res.cloudinary.com/dcss55nem/image/upload/v1747625577/Screenshot_2025-05-18_at_8.23.59_PM_fvumyy.png",
+      title: "Dashboard",
       paragraph: (
         <ul>
           Created custom dashboard components using the recharts library.
@@ -70,17 +70,9 @@ function Project1() {
       ),
     },
     {
-      opacity: 0.5,
-      imageUrl:
-        "https://res.cloudinary.com/dcss55nem/image/upload/v1714515630/DashboardPagePart2_kgv66o.png",
-      title: "Dashboard Part 2",
-      paragraph:
-        "Line graph created using recharts, displaying the number of students enrolled over time, along with additional reusable card components.",
-    },
-    {
       opacity: 0.3,
       imageUrl:
-        "https://res.cloudinary.com/dcss55nem/image/upload/v1714515574/StudentPage_jhq9nt.png",
+        "https://res.cloudinary.com/dcss55nem/image/upload/v1747625347/Screenshot_2025-05-18_at_8.24.52_PM_qelt8k.png",
       title: "Student Page",
       paragraph: (
         <ul>
@@ -106,7 +98,7 @@ function Project1() {
     {
       opacity: 0.3,
       imageUrl:
-        "https://res.cloudinary.com/dcss55nem/image/upload/v1703106153/Screenshot_47_tdnhsh.png",
+        "https://res.cloudinary.com/dcss55nem/image/upload/v1747625356/Screenshot_2025-05-18_at_8.25.06_PM_nlyckk.png",
       title: "Student Profile Part 1",
       paragraph: (
         <ul>
@@ -127,7 +119,7 @@ function Project1() {
     {
       opacity: 0.4,
       imageUrl:
-        "https://res.cloudinary.com/dcss55nem/image/upload/v1703262379/Screenshot_48_rvdabm.png",
+        "https://res.cloudinary.com/dcss55nem/image/upload/v1747625364/Screenshot_2025-05-18_at_8.25.45_PM_l9wfbo.png",
       title: "Student Profile Part 2",
       paragraph:
         "Collapsible headers provide useful information about the student, program, parent, or other entities.",
@@ -135,17 +127,17 @@ function Project1() {
     {
       opacity: 0.4,
       imageUrl:
-        "https://res.cloudinary.com/dcss55nem/image/upload/v1703262475/Screenshot_57_ll3bll.png",
+        "https://res.cloudinary.com/dcss55nem/image/upload/v1747625378/Screenshot_2025-05-18_at_8.27.30_PM_rdolf3.png",
       title: "Student Profile Part 3",
       paragraph:
-        "Created a reusable modal component used throughout the web application.",
+        "Notes section allows users to add notes about the student.",
     },
     {
       opacity: 0.4,
       imageUrl:
-        "https://res.cloudinary.com/dcss55nem/image/upload/v1703106160/Screenshot_49_cy3fcs.png",
+        "https://res.cloudinary.com/dcss55nem/image/upload/v1747625386/Screenshot_2025-05-18_at_8.26.25_PM_lokv3k.png",
       title: "Student Profile Part 4",
-      paragraph: "Includes a reusable Delete component.",
+      paragraph: "Created a custom reusable modal",
     },
     {
       opacity: 0.5,
