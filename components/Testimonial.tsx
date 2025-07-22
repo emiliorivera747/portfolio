9754ee3bafdedc841efdb543184ec39313c6e0a2
Emilio Rivera <emiliorivera747@gmail.com>
2025-07-22 07:21:50 -0700

    Fixed testimonial layout

--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -3,15 +3,15 @@ import { CldImage } from 'next-cloudinary';
 
 const Testimonial = () => {
   return (
-    <section className=" relative h-[100vh] w-screen border-b-2 border-zinc-100 overflow-y-scroll">
-      <div className=" flex items-center justify-center text-center">
+    <section className="relative min-h-[80vh] w-screen border-b-2 border-[#dee2e6] overflow-y-scroll flex flex-col items-center justify-center gap-10">
+      <div className="flex items-center justify-center text-center">
         <h1 className="text-primary-1000 text-3xl pt-10 font-semibold">
           Client Testimonials
         </h1>
       </div>
-      <blockquote className="absolute  px-6 pt-10 md:px-18  flex md:flex-col flex-col h-auto gap-4 sm:pt-2">
-        <h2 className=" text-lg md:text-[1.6rem] leading-loose tracking-wider md:mx-44 md:pt-12 md:pb-6 flex rounded  font-extralight">
-          <p className="  from-primary-900 to-primary-800 bg-gradient-to-r bg-clip-text text-transparent px-2 flex text-center justify-center items-center leading-loose tracking-wider font-thin ">
+      <blockquote className="px-6 pt-10 md:px-18  flex md:flex-col flex-col h-auto gap-4 sm:pt-2">
+        <h2 className=" text-lg md:text-[1.6rem] leading-loose tracking-wider md:mx-44 md:pt-12 md:pb-12 flex rounded  font-extralight">
+          <p className="  from-primary-900 to-primary-700 bg-gradient-to-r bg-clip-text text-transparent px-2 flex text-center justify-center items-center leading-loose tracking-wider font-thin ">
             Emilio will pour his dedication, passion, and intelligence towards creating solutions with the client&apos;s best interests in mind. This was my experience. Emilio would be an incredible asset to any company, client, and/or organization.
           </p>
         </h2>
