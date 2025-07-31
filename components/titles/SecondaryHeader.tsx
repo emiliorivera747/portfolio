1f2c563b99a5d787309d15ae40600ecf2f17c20d
Emilio Rivera <emiliorivera747@gmail.com>
2025-07-30 20:06:10 -0700

    added comments to Secondary Header

--- components/titles/SecondaryHeader.tsx
+++ components/titles/SecondaryHeader.tsx
@@ -2,6 +2,9 @@ import React, { forwardRef } from 'react';
 import {cn} from '@/lib/utils';
 import { SecondaryHeaderProps } from '@/types/headers';
 
+/**
+ * Added Secondary header that can be customized
+ */
 const SecondaryHeader = forwardRef<HTMLDivElement, SecondaryHeaderProps>(
   ({ title, className }, ref) => {
     const defaultClasses = "font-semibold text-3xl text-primary-1000";
