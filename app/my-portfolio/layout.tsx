41980ab0b270e6ee6c3eccf52acfc3ae2f451c76
Emilio Rivera <emiliorivera747@gmail.com>
2025-07-31 19:50:20 -0700

    Added comments to my-porfolio layout page

--- app/my-portfolio/layout.tsx
+++ app/my-portfolio/layout.tsx
@@ -4,6 +4,13 @@ export const metadata = {
     "Emilio created an internal employee web application using MongoDB, Express.js, Node.js, and React to help manage programs for Casa Chirilagua, a non-profit organization based in the City of Alexandria, Virginia"
 };
 
+/**
+ * 
+ * The layout page for my-portfolio
+ * 
+ * @param {React.ReactNode} children
+ * @returns layout
+ */
 export default function RootLayout({
   children,
 }: {
