54bb30a59860a674b9525daa6effe6ccd3148081
Emilio Rivera <emiliorivera747@gmail.com>
2025-07-31 19:51:48 -0700

    Added comments to my portfolio page

--- app/my-portfolio/page.tsx
+++ app/my-portfolio/page.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
+
+/**
+ * 
+ * My portfolio page
+ * 
+ * @returns portfolio page
+ */
 const page = () => {
   return (
     <section className='bg-black text-white flex items-center justify-center h-screen w-screen font-bold text-5xl'>Coming soon...</section>
