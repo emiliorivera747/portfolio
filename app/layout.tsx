cdde4d4027fb3bc888fb96b81855205d1a0b1177
Emilio Rivera <emiliorivera747@gmail.com>
2025-08-03 15:22:30 -0700

    Added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,23 @@
-
 import "../styles/globals.css";
+import Script from "next/script";
 
 export const metadata = {
   icons: {
-    icon: '/favicon.png',
+    icon: "/favicon.png",
   },
   title: "Home | Emilio Rivera's Portfolio",
   description:
     "Welcome to Emilio Rivera's Portfolio, a Software Engineer based in San Jose, CA. specializing in React.js, Next.js, and Node.js.",
 };
 
-
 //Data
 import { navBarData } from "@/utils/data/navbar/navbarData";
 import Navbar from "../components/navbar/Navbar";
 
-
 /**
- * 
- * The root layout for all of the pages 
- * 
+ *
+ * The root layout for all of the pages
+ *
  * @param children - the child componeent in this case woul be the page.tsx
  * @returns root layout
  */
@@ -28,9 +26,21 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-R0GTFSV0LN"
+        ></Script>
+        <Script id="google-analytics">
+          {`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-R0GTFSV0LN');`}
+        </Script>
+      </head>
       <body>
         <Navbar menuItems={navBarData} />
         {children}
