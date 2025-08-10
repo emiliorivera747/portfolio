c7172183332e01c0d57b25eb9a6883d1335fc0a7
Emilio Rivera <emiliorivera747@gmail.com>
2025-08-09 18:28:12 -0700

    Added test API route

---
+++ app/api/hello-world/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export async function GET() {
+  try {
+    return NextResponse.json({
+      status: "success",
+      message: "Hello World!",
+    });
+  } catch (error) {
+    const errorMessage =
+      error instanceof Error ? error.message : "unknown error";
+    return NextResponse.json(
+      {
+        message: errorMessage,
+        data: null,
+      },
+      { status: 500 }
+    );
+  }
+}
