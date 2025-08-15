04aaf808a74fdf9176885fd886e46ea32031d918
Emilio Rivera <emiliorivera747@gmail.com>
2025-08-14 23:08:17 -0700

    Added comments to PasswordInput component

--- components/form-components/PasswordInput.tsx
+++ components/form-components/PasswordInput.tsx
@@ -8,11 +8,17 @@ import PrimaryInputLabel from "@/components/form-components/PrimaryInputLabel";
 import PasswordTooltip from "@/features/auth/components/tooltips/PasswordTooltip";
 import ErrorForInputs from "@/components/errors/ErrorForInputs";
 
-
 //Types
 import { PasswordInputProps } from "@/types/forms";
 
-
+/**
+ *
+ * Displays password input
+ *
+ * @param id 
+ * @param placeholder
+ * @returns
+ */
 const PasswordInput = <TFieldValues extends FieldValues>({
   id = `password-input`,
   placeholder = "Password",
@@ -20,8 +26,7 @@ const PasswordInput = <TFieldValues extends FieldValues>({
   errors,
   register,
   withPasswordTooltip = false,
-}: 
-PasswordInputProps<TFieldValues>) => {
+}: PasswordInputProps<TFieldValues>) => {
   const [showPassword, setShowPassword] = useState(false);
   const [password, setPassword] = useState("");
   const [isFocused, setIsFocused] = useState(false);
@@ -46,52 +51,58 @@ PasswordInputProps<TFieldValues>) => {
   return (
     <div className="relative my-1">
       <input
-      type={showPassword ? "text" : "password"}
-      id={id}
-      {...register(fieldName, {
-        onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (setPassword) setPassword(e.target.value);
-        },
-      })}
-      className={`border-box rounded-[12px] align-text-bottom w-full px-[1rem] pt-[1.05882rem] h-[3.2941176471rem] border leading-[1.23536] ${
-        errors[fieldName] ? "border-red-500 bg-[#fff5f5] text-red-500" : "border-tertiary-600"
-      } rounded-[12px] focus:outline-none focus:ring-2 ${
-        errors[fieldName] ? "focus:ring-red-500" : "focus:ring-primary-500 focus:border-none"
-      } peer placeholder-transparent `}
-      placeholder={placeholder}
-      defaultValue=""
-      onFocus={() => {
-        if (setIsFocused) setIsFocused(true);
-      }}
+        type={showPassword ? "text" : "password"}
+        id={id}
+        {...register(fieldName, {
+          onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
+            if (setPassword) setPassword(e.target.value);
+          },
+        })}
+        className={`border-box rounded-[12px] align-text-bottom w-full px-[1rem] pt-[1.05882rem] h-[3.2941176471rem] border leading-[1.23536] ${
+          errors[fieldName]
+            ? "border-red-500 bg-[#fff5f5] text-red-500"
+            : "border-tertiary-600"
+        } rounded-[12px] focus:outline-none focus:ring-2 ${
+          errors[fieldName]
+            ? "focus:ring-red-500"
+            : "focus:ring-primary-500 focus:border-none"
+        } peer placeholder-transparent `}
+        placeholder={placeholder}
+        defaultValue=""
+        onFocus={() => {
+          if (setIsFocused) setIsFocused(true);
+        }}
       />
-      {withPasswordTooltip && isFocused && password && <PasswordTooltip password={password} />}
+      {withPasswordTooltip && isFocused && password && (
+        <PasswordTooltip password={password} />
+      )}
 
       {/* Password Visibility */}
       <div
-      className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer h-[3.2941176471rem]"
-      onClick={togglePasswordVisibility}
+        className="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer h-[3.2941176471rem]"
+        onClick={togglePasswordVisibility}
       >
-      {showPassword ? (
-        <FaEyeSlash
-        className={`${
-          errors[fieldName] ? "text-red-500" : "text-[#868e96]"
-        }`}
-        />
-      ) : (
-        <FaEye
-        className={`${
-          errors[fieldName] ? "text-red-500" : "text-[#868e96]"
-        }`}
-        />
-      )}
+        {showPassword ? (
+          <FaEyeSlash
+            className={`${
+              errors[fieldName] ? "text-red-500" : "text-[#868e96]"
+            }`}
+          />
+        ) : (
+          <FaEye
+            className={`${
+              errors[fieldName] ? "text-red-500" : "text-[#868e96]"
+            }`}
+          />
+        )}
       </div>
 
       {/* Placeholder label*/}
       <PrimaryInputLabel
-      id={id}
-      fieldName={fieldName}
-      placeholder={placeholder}
-      errors={errors}
+        id={id}
+        fieldName={fieldName}
+        placeholder={placeholder}
+        errors={errors}
       />
 
       {/* Error message */}
