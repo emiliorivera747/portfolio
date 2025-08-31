50677cac2de8973e1f3ec2c3d5be17a71227c6b7
Emilio Rivera <emiliorivera747@gmail.com>
2025-08-30 21:01:14 -0700

    Added comments to sign in loading skeleton

--- app/sign-in/loading.tsx
+++ app/sign-in/loading.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import SigninPageSkeleton from '@/components/skeletons/pages/SigninPageSkeleton'
 
+/**
+ *  Sign in loading page
+ * 
+ * @returns Sign in loading skeleton
+ */
 const loading = () => {
   return (
     <SigninPageSkeleton />
