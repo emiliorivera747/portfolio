ea6fbb447736ea392a5770d9de34e3e0158cb5a4
Emilio Rivera <emiliorivera747@gmail.com>
2025-09-01 21:40:26 -0700

    Added Meta Data for sign in page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Sign In | Trellis Money",
-  description: "Sign in to your Trellis Money account",
+  title: "Sign In | Emilio Rivera's Portfolio",
+  description: "Sign in to your Emilio's Portfolio account",
 };
 
-//Forms
 import SignInForm from "@/features/auth/components/form/SignInForm";
 
+/**
+ *
+ * @returns
+ */
 const LoginPage = () => {
   return (
     <div className="flex flex-col items-center justify-center m-h-screen h-auto min-w-screen">
