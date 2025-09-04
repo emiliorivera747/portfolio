0dd69485255dc3db8c0144cf3e9c6f7aa2844461
Emilio Rivera <emiliorivera747@gmail.com>
2025-09-03 21:36:27 -0700

    removed nav from sign in page skeleton

--- components/skeletons/pages/SigninPageSkeleton.tsx
+++ components/skeletons/pages/SigninPageSkeleton.tsx
@@ -8,11 +8,11 @@ import PrimaryAuthContainer from "@/features/auth/components/containers/PrimaryA
 const SigninPageSkeleton = () => {
   return (
     <div className="animate-pulse">
-      <nav className="flex justify-between p-4 border-b border-gray-200 mx-10">
+      {/* <nav className="flex justify-between p-4 border-b border-gray-200 mx-10">
         <div className="flex items-center">
           <div className="h-10 w-32 bg-gray-300 rounded"></div>
         </div>
-      </nav>
+      </nav> */}
       <div className="flex flex-col items-center justify-center min-h-screen h-auto min-w-screen w-auto">
         <PrimaryAuthContainer>
           <div className="">
