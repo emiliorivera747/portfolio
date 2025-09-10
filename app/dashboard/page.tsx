7fab45d8a63f95d9a40c5c9a2c0cc60ba22abd6d
Emilio Rivera <emiliorivera747@gmail.com>
2025-09-10 15:02:42 -0700

    Styled dashboard page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Dashboard</div>
+    <div className='flex items-center justify-center h-screen w-screen text-4xl font-semibold'>Dashboard</div>
   )
 }
 
