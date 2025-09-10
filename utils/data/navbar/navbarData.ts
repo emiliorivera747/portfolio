64f665b5230c2a9f7feca3c4de0dc2348dfc86ea
Emilio Rivera <emiliorivera747@gmail.com>
2025-09-10 15:09:57 -0700

    Added Dashboard to nav bar

--- utils/data/navbar/navbarData.ts
+++ utils/data/navbar/navbarData.ts
@@ -1,53 +1,65 @@
 export const navBarData = [
-    {
-      id: 2,
-      label: "Projects",
-      url: "/projects",
-      content: [
-        {
-          id: 1,
-          label: "Casa Chirilagua",
-          url: "/casa-chirilagua",
-        },
-        {
-          id: 2,
-          label: "Portfolio",
-          url: "/my-portfolio",
-        },
-        {
-          id: 3,
-          label: "All Projects",
-          url: "/projects",
-        },
-      ],
-    },
-    {
-      id: 3,
-      label: "About",
-      url: "/about",
-      content: [
-        {
-          id: 1,
-          label: "About Me",
-          url: "/about",
-        },
-        {
-          id: 2,
-          label: "Contact",
-          url: "/#",
-        },
-      ],
-    },
-    {
-      id:4,
-      label: 'Sign In',
-      url:'/sign-in',
-      content: [
-        {
-          id: 1,
-          label: "Sign In",
-          url: "/sign-in",
-        },
-      ],
-    }
-  ];
+  {
+    id: 2,
+    label: "Projects",
+    url: "/projects",
+    content: [
+      {
+        id: 1,
+        label: "Casa Chirilagua",
+        url: "/casa-chirilagua",
+      },
+      {
+        id: 2,
+        label: "Portfolio",
+        url: "/my-portfolio",
+      },
+      {
+        id: 3,
+        label: "All Projects",
+        url: "/projects",
+      },
+    ],
+  },
+  {
+    id: 3,
+    label: "About",
+    url: "/about",
+    content: [
+      {
+        id: 1,
+        label: "About Me",
+        url: "/about",
+      },
+      {
+        id: 2,
+        label: "Contact",
+        url: "/#",
+      },
+    ],
+  },
+  {
+    id: 5,
+    label: "Dashboard",
+    url: "/dashboard",
+    content: [
+      {
+        id: 1,
+        label: "Dashboard",
+        url: "/dashboard",
+      },
+    ],
+  },
+  {
+    id: 4,
+    label: "Sign In",
+    url: "/sign-in",
+    content: [
+      {
+        id: 1,
+        label: "Sign In",
+        url: "/sign-in",
+      },
+    ],
+  },
+];
