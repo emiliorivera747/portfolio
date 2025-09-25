8af257ad1c232df381502d9d85f81e2042dd36a7
Emilio Rivera <emiliorivera747@gmail.com>
2025-09-25 10:30:08 -0700

    Updated the Public Speaking qoute

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -113,8 +113,9 @@ function About() {
         </section>
         <div className="bg-white p-10 flex flex-col md:flex-col h-auto min-h-[70vh] text-center items-center justify-center gap-0 ">
           <h2 className=" bg-gradient-to-r bg-clip-text text-transparent from-primary-1000 to-primary-800 text-4xl md:p-10 leading-loose tracking-wider pb-10">
-            {`"Everyone has a story, an idea, or message to share and there is no one better than for you to share it through`}{" "}
-            <strong>Public Speaking</strong> <span>{`"`}</span>
+            {`"Everyone has a story, an idea, or message to share with the world and there is no one better than for you to share it through`}{" "}
+            <strong>Public Speaking</strong>
+            <span>{`"`}</span>
           </h2>
           <p className="text-xl font-light text-[#868e96]">- Emilio Rivera</p>
         </div>
