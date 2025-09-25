1a41bdee84acb2c1b5b0a9edb684b3e637391190
Emilio Rivera <emiliorivera747@gmail.com>
2025-09-25 09:49:07 -0700

    Text editor outputs html and json

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -7,7 +7,7 @@ import { useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 
 // Components
-import Tiptap from "@/components/tiptap/RichTextEditor";
+import RichTextEditor from "@/components/tiptap/RichTextEditor";
 
 // Removed unused import BackToButton
 import NavbarLogo from "@/components/navbar/NavbarLogo";
@@ -29,41 +29,22 @@ const Page = () => {
       Highlight,
     ],
     immediatelyRender: false,
-    content: `
-      <h3 style="text-align:center">
-        Devs Just Want to Have Fun by Cyndi Lauper
-      </h3>
-      <p style="text-align:center">
-        I come home in the morning light<br>
-        My mother says, <mark>“When you gonna live your life right?”</mark><br>
-        Oh mother dear we’re not the fortunate ones<br>
-        And devs, they wanna have fun<br>
-        Oh devs just want to have fun</p>
-      <p style="text-align:center">
-        The phone rings in the middle of the night<br>
-        My father yells, "What you gonna do with your life?"<br>
-        Oh daddy dear, you know you’re still number one<br>
-        But <s>girls</s>devs, they wanna have fun<br>
-        Oh devs just want to have
-      </p>
-      <p style="text-align:center">
-        That’s all they really want<br>
-        Some fun<br>
-        When the working day is done<br>
-        Oh devs, they wanna have fun<br>
-        Oh devs just wanna have fun<br>
-        (devs, they wanna, wanna have fun, devs wanna have)
-      </p>
-    `,
+    content: "",
     editorProps: {
       attributes: {
         class:
-          "prose prose-sm sm:prose-base lg:prose-lg xl:prose-2xl border  w-[40rem] border-primary-600 rounded-[12px] p-8",
+          "prose prose-sm sm:prose-base lg:prose-lg xl:prose-2xl border  w-[40rem] border-primary-600 rounded-[12px] p-8 bg-zinc-50",
       },
+    },
+    onUpdate: ({ editor }) => {
+      const html = editor.getHTML();
+      const json = editor.getJSON();
+      console.log("Content updated", { html, json });
     },
   });
 
   if (!editor) return null;
+
   return (
     <section className="h-screen w-screen">
       <div className="grid grid-cols-[1fr_10fr] h-full w-full ">
@@ -75,11 +56,11 @@ const Page = () => {
         </div>
 
         <div className="px-10 flex flex-col items-center mt-20 ">
-          <h2 className="font-semibold text-3xl text-transparent bg-clip-text bg-gradient-to-r to-primary-700 from-primary-900 mb-10">
+          <h2 className="font-semibold text-3xl text-transparent bg-clip-text bg-gradient-to-r to-primary-800 from-primary-900 mb-10">
             Create post
           </h2>
           <MenuBar editor={editor} />
-          <Tiptap editor={editor} />
+          <RichTextEditor editor={editor} />
         </div>
       </div>
     </section>
