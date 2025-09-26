6e83bb43df85f7357952157071dd8657667490dc
Emilio Rivera <emiliorivera747@gmail.com>
2025-09-26 10:26:45 -0700

    Added navigation bar type

---
+++ types/navigationbar.ts
@@ -0,0 +1,10 @@
+export interface SideNavItemLinkProps {
+  href: string;
+  svg_d: string;
+  currentPath: string;
+  label: string;
+}
+export interface TooltipProps {
+  title: string;
+  children: React.ReactNode;
+}
