860dee50be5dce519d6da88ac434131af0dcf4e6
Emilio Rivera <emiliorivera747@gmail.com>
2025-10-08 09:12:01 -0700

    Added default placeholder for blog admin section

--- app/admin/blogs/page.tsx
+++ app/admin/blogs/page.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const pasge = () => {
-  return <div>page</div>;
+/**
+ * 
+ * Displays all blogs and should allow the user to edit
+ * 
+ * @returns 
+ */
+const page = () => {
+  return <section className="flex items-center justify-center h-screen w-full">Blog</section>;
 };
 
-export default pasge;
+export default page;
