b0ad627297b0423d9064a12e6712269e5c527428
Emilio Rivera <emiliorivera747@gmail.com>
2025-10-08 12:49:13 -0700

    Added blogs page

---
+++ app/blogs/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <section className='w-screen min-h-screen flex items-center justify-center font-bold text-2xl'>
+        In the works...
+    </section>
+  )
+}
+
+export default page
