86e6493ee34d04316bad9dbce4da6527a80bfb8a
Emilio Rivera <emiliorivera747@gmail.com>
2025-10-08 10:30:49 -0700

    Added title to Post Schema

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -8,7 +8,6 @@ import { NextResponse, NextRequest } from "next/server";
 export async function POST(req: NextRequest) {
   
   const body = await req.json();
-  console.log(body);
 
   try {
     return NextResponse.json(
