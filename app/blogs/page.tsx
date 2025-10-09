5a0c696e3be4843afe914cd313691bf531eabf13
Emilio Rivera <emiliorivera747@gmail.com>
2025-10-08 22:42:53 -0700

    Added content to the blogs page

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -4,11 +4,14 @@ import useFetchBlogs from "@/features/blogs/hooks/useFetchBlogs";
 
 const Page = () => {
   const { postResponse, isLoadingPosts, isErrorPosts } = useFetchBlogs();
-  console.log(postResponse)
+  console.log(postResponse);
   return (
-    <section className="w-screen min-h-screen flex items-center justify-center font-bold text-2xl">
+    <section className="w-screen min-h-screen">
+      <div className='text-primary-1000 text-4xl pt-[12rem] px-14 pb-4 font-semibold'>In the works...</div>
       {postResponse?.data.map(({ title }: { title: string }) => (
-        <div key={title} className="text-priamry-900">{title}</div>
+        <div key={title} className="text-primary-900 px-14">
+          {title}
+        </div>
       ))}
     </section>
   );
