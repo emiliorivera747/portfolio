80384a73e90f988fe613bbbf74673871f41fff09
Emilio Rivera <emiliorivera747@gmail.com>
2025-10-09 10:16:31 -0700

    Protect api route

--- middleware.ts
+++ middleware.ts
@@ -10,7 +10,7 @@ export const config = {
     "/admin/dashboard",
     "/admin/:path*",
     "/admin/:path*",
-    // '/api/:path*',
+    '/api/:path*',
     /*
      * Match all request paths except for the ones starting with:
      * - _next/static (static files)
