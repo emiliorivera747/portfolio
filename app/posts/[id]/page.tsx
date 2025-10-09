5d8bfa436c6d61b51bc6c53f4e2cb2dd4cd343a0
Emilio Rivera <emiliorivera747@gmail.com>
2025-10-09 09:38:16 -0700

    Fixed async issue on post with id page

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 
-const page = ({ params }: { params: { id: String } }) => {
+const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
   return (
     <section className="h-screen w-screen flex flex-col items-center justify-center text-xl font-bold">
       <h1>Page is in the works!</h1>
-      <span className="text-primary-700 font-light">post id: {params.id}</span>
+      <span className="text-primary-700 font-light">post id: {id}</span>
     </section>
   );
 };
 
-export default page;
+export default Page;
