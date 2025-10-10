5c1351fbd2c35e2fdb86b861d9c9fd12120c7b22
Emilio Rivera <emiliorivera747@gmail.com>
2025-10-10 12:13:08 -0700

    Added posts types

--- features/blogs/types/post.ts
+++ features/blogs/types/post.ts
@@ -1,13 +1,41 @@
 export interface Post {
-  id: number;
+  id?: number;
   user_id?: number;
-  created_at: Date;
-  updated_at: Date;
+  created_at?: Date;
+  updated_at?: Date;
   description?: string;
   title?: string;
   image_url?: string;
 }
+export interface ContentBlock {
+  id: number;
+  content_order: number;
+  content_type: string; 
+  content_data: Record<string, any>;
+  post_id: number;
+  media_id?: number;
+  created_at: Date;
+  updated_at: Date;
+}
 
-export interface PostListProps{
-    posts: Post[];
+export interface PostWithRelations extends Post {
+  User?: {
+    id: number;
+    name?: string;
+  };
+  content_block: ContentBlock[];
+  comments?: {
+    id: number;
+    text: string;
+    created_at: Date;
+    updated_at: Date;
+  }[];
+  PostTag?: {
+    id: number;
+    tag_name: string;
+  }[];
+}
+
+export interface PostListProps {
+  posts: Post[];
 }
