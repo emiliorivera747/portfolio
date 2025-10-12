d0ea5d0eff5180fed7f0b64070f13cb7d3ed44b6
Emilio Rivera <emiliorivera747@gmail.com>
2025-10-12 08:51:37 -0700

    Made use of reuable loading page

--- app/about/loading.tsx
+++ app/about/loading.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import DotLoader from "@/components/loading/DotLoader";
+import LoadingPage from "@/components/loading/LoadingPage";
 
 /**
  *
@@ -8,23 +8,7 @@ import DotLoader from "@/components/loading/DotLoader";
  * @returns loading page
  */
 const loading = () => {
-  return (
-    <div className="flex items-center justify-center bg-white h-screen w-screen text-white ">
-      <div className="w-screen h-screen flex flex-col gap-2 items-center justify-center">
-        <div className="translate-y-[-50%] flex flex-col items-center justify-center">
-          <div className="text-primary-1000 text-md mb-4 h-[90%]">
-            <span className="font-semibold text-xl">
-              Simplicity is the ultimate sophistication.
-            </span>{" "}
-            <p className="flex items-center justify-center text-primary-700 mt-4">
-              - Leonardo Da Vinci
-            </p>
-          </div>
-          <DotLoader bgColor="bg-primary-1000" dotWidth="w-3" dotHeight="h-3" />
-        </div>
-      </div>
-    </div>
-  );
+  return <LoadingPage theme="white"/>;
 };
 
 export default loading;
