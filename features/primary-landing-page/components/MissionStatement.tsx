1e7db7c91047b1df889d0300ca85fa5bd1cb28a1
Emilio Rivera <emiliorivera747@gmail.com>
2025-10-12 14:47:47 -0700

    Updated Mission statement line spacing

--- features/primary-landing-page/components/MissionStatement.tsx
+++ features/primary-landing-page/components/MissionStatement.tsx
@@ -7,7 +7,7 @@
  */
 const MissionStatement = () => {
   return (
-    <p  className="sm:w-2/3 pl-1 pb-4 sm:pb-10 text-primary-200 font-light mission-statement text-sm sm:text-md">
+    <p  className="sm:w-2/3 pl-1 pb-4 sm:pb-10 text-primary-200 font-normal mission-statement text-sm sm:text-[1.1rem] leading-relaxed">
       Develop minimalist web and front-end applications using modern frameworks,
       tailored for small businesses and non-profit organizations.
     </p>
