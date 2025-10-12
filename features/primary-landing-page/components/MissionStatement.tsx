2bc2d46a1e5e436d54cce9f288cf7714547e8cab
Emilio Rivera <emiliorivera747@gmail.com>
2025-10-12 08:45:11 -0700

    Made text on langing page responsive

--- features/primary-landing-page/components/MissionStatement.tsx
+++ features/primary-landing-page/components/MissionStatement.tsx
@@ -7,7 +7,7 @@
  */
 const MissionStatement = () => {
   return (
-    <p  className="sm:w-2/3 pl-1 pb-4 sm:pb-10 text-primary-200 font-light mission-statement sm:text-md">
+    <p  className="sm:w-2/3 pl-1 pb-4 sm:pb-10 text-primary-200 font-light mission-statement text-sm sm:text-md">
       Develop minimalist web and front-end applications using modern frameworks,
       tailored for small businesses and non-profit organizations.
     </p>
--- features/primary-landing-page/components/Role.tsx
+++ features/primary-landing-page/components/Role.tsx
@@ -9,7 +9,7 @@ const Role = ({ role }: RoleProp) => {
   return (
     <div className="role-heading">
       <h1
-        className="font-semibold bg-gradient-to-r from-primary-500 to-primary-1000 bg-clip-text text-transparent text-2xl sm:text-[1.7rem]"
+        className="font-semibold bg-gradient-to-r from-primary-500 to-primary-1000 bg-clip-text text-transparent text-xl sm:text-[1.7rem]"
       >
         {role}
       </h1>
