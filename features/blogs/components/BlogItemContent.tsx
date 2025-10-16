0a820b392c6f98efa04ccf285c7f059f08a9c6e3
Emilio Rivera <emiliorivera747@gmail.com>
2025-10-15 17:23:47 -0700

    Added reading time for post

--- features/blogs/components/BlogItemContent.tsx
+++ features/blogs/components/BlogItemContent.tsx
@@ -15,17 +15,39 @@ const BlogItemContent = ({
   createdAt = new Date(),
 }: BlogItemContent) => {
   return (
-    <div className="w-[20rem] flex flex-col gap-4">
+    <div className="w-[20rem] flex flex-col gap-1">
       <Link
         href={`posts/${id}`}
-        className="font-semibold text-primary-1000 text-2xl hover:underline"
+        className="font-bold text-primary-1000 text-3xl hover:underline"
       >
         {title}
       </Link>
-      <div className="font-normal text-primary-700  flex flex-row text-md gap-1">
-        <span>Emilio Rivera </span>
-        <span className="">-</span>
-        <span className="">
+      <span className="text-primary-800 font-extralight text-md mb-4 ">
+        {description ? description : "No description provided for the post"}
+      </span>
+      <div className="font-light text-primary-700  flex flex-col text-md gap-2">
+        <span className="text-primary-800 text-md">
+          <i>By Emilio Rivera</i>
+        </span>
+        <span className="flex flex-row gap-4 items-center text-sm">
+          <div className="flex items-center gap-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-5 "
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            <span>1 min read</span>
+          </div>
+
           {new Date(createdAt).toLocaleDateString("en-US", {
             month: "short",
             day: "2-digit",
@@ -33,10 +55,6 @@ const BlogItemContent = ({
           })}
         </span>
       </div>
-
-      <span className="text-primary-700 font-extralight text-md">
-        {description ? description : "No description provided for the post"}
-      </span>
     </div>
   );
 };
