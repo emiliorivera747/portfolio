3d6eb5255c2749ec6c4aae06a9b8fdefa2f3594e
Emilio Rivera <emiliorivera747@gmail.com>
2025-10-24 12:17:32 -0700

    Added params to Editor with menu

--- components/tiptap/EditorWithMenu.tsx
+++ components/tiptap/EditorWithMenu.tsx
@@ -5,25 +5,30 @@ import RichTextEditor from "@/components/tiptap/RichTextEditor";
 // Hooks
 import useBlogEditor from "@/features/blog-composer/hooks/useBlogEditor";
 
+
+interface UseBlogEditorProps {
+    id: string;
+    initialContent: string;
+    onUpdate: (id: string, newContent: string) => void;
+  }
 /**
  * Displays the editor with both menu bar and rich text editor
  *
  * @param param0
  * @returns
  */
-const EditorWithMenu = ({ isActive = true }: { isActive?: boolean }) => {
-  const { editor, blogContent } = useBlogEditor();
+const EditorWithMenu = ({
+    id,
+    initialContent,
+    onUpdate,
+  }: UseBlogEditorProps) => {
+  const { editor, blogContent } = useBlogEditor({ id: id, initialContent: initialContent, onUpdate: onUpdate });
   if (!editor) return null;
 
   return (
     <div className="w-full mb-8">
-      {isActive && (
-        <>
-          <MenuBar editor={editor} />
-          <RichTextEditor editor={editor} />
-        </>
-      )}
-      {!isActive && }
+      <MenuBar editor={editor} />
+      <RichTextEditor editor={editor} />
     </div>
   );
 };
