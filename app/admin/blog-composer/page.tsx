ebbe545dfb352fabf894752b73c7ec78a92d291c
Emilio Rivera <emiliorivera747@gmail.com>
2025-10-24 10:35:52 -0700

    Created PostForm component

--- app/admin/blog-composer/page.tsx
+++ app/admin/blog-composer/page.tsx
@@ -14,6 +14,7 @@ import PostContentSelect from "@/features/blog-composer/components/post-content/
 import FormFieldGenerator from "@/components/form-builder/FormFieldGenerator";
 import SecondaryHeader from "@/features/blog-composer/components/headings/SecondaryHeading";
 import PostContent from "@/features/blog-composer/components/post-content/PostContent";
+import PostForm from "@/features/blog-composer/components/post-form/PostForm";
 
 // Zod Schemas
 import {
@@ -36,59 +37,16 @@ import { fields } from "@/features/blog-composer/data/formFields";
  *
  */
 const Page = () => {
-  const buttonRef = useRef(null);
-  const [open, setOpen] = useState(false);
   const { editor, blogContent } = useBlogEditor();
-  const [contentBlock, setContentBlock] = useState([]);
-  const { handleFileChange, file } = useFile();
-
-  const form = useForm<FormSchema>({
-    resolver: zodResolver(formSchema),
-  });
-
   const { onSubmit } = useSubmitPost({ editor: editor });
 
   if (!editor) return null;
 
   return (
     <section className="w-full max-h-screen overflow-y-scroll">
+      {/* Form to show editor */}
       <div className="h-auto flex items-center justify-center mb-10 mx-[6%]">
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="flex flex-col items-center  w-[40rem] pt-[4rem]"
-          >
-            <PrimaryHeader title={"Create Post"} />
-            <div className="w-full flex flex-col mb-10">
-              <SecondaryHeader label="Details" />
-              <FormFieldGenerator
-                fields={fields.map((field) => ({
-                  ...field,
-                  control: form.control as any,
-                }))}
-              />
-              <UploadButton
-                data-testid="upload-button"
-                handleFileChange={handleFileChange}
-                open={open}
-                setOpen={setOpen}
-              />
-            </div>
-
-            <PostContentSelect />
-
-            {/* <div className="w-full mb-8">
-              <MenuBar editor={editor} />
-              <RichTextEditor editor={editor} />
-            </div> */}
-
-            <PrimarySubmitButton
-              className="mt-4"
-              ref={buttonRef}
-              text="Publish"
-            />
-          </form>
-        </Form>
+        <PostForm editor={editor} onSubmit={onSubmit} />
       </div>
 
       {/* Preview of the Post Content */}
---
+++ features/blog-composer/components/post-form/PostForm.tsx
@@ -0,0 +1,86 @@
+import React, { useState, useRef } from "react";
+
+// External Lib
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+// Components
+import PrimarySubmitButton from "@/components/buttons/PrimarySubmitButton";
+import PrimaryHeader from "@/components/headers/PrimaryHeader";
+import { Form, FormField } from "@/components/ui/form";
+import UploadButton from "@/components/form-components/UploadButton";
+import PostContentSelect from "@/features/blog-composer/components/post-content/PostContentSelect";
+import FormFieldGenerator from "@/components/form-builder/FormFieldGenerator";
+import SecondaryHeader from "@/features/blog-composer/components/headings/SecondaryHeading";
+import PostContent from "@/features/blog-composer/components/post-content/PostContent";
+
+// Zod Schemas
+import {
+  formSchema,
+  FormSchema,
+} from "@/features/blog-composer/schemas/composerSchemas";
+
+// Hooks
+import useSubmitPost from "@/features/blog-composer/hooks/useSubmitPost";
+import useBlogEditor from "@/features/blog-composer/hooks/useBlogEditor";
+import useFile from "@/hooks/useFile";
+
+// Data
+import { fields } from "@/features/blog-composer/data/formFields";
+
+// Types
+import { Editor } from "@tiptap/react";
+
+const PostForm = ({
+  editor,
+  onSubmit,
+}: {
+  editor: Editor | null;
+  onSubmit: (data: FormSchema) => void;
+}) => {
+  const buttonRef = useRef(null);
+
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const { handleFileChange, file } = useFile();
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col items-center  w-[40rem] pt-[4rem]"
+      >
+        <PrimaryHeader title={"Create Post"} />
+        <div className="w-full flex flex-col mb-10">
+          <SecondaryHeader label="Details" />
+          <FormFieldGenerator
+            fields={fields.map((field) => ({
+              ...field,
+              control: form.control as any,
+            }))}
+          />
+          <UploadButton
+            data-testid="upload-button"
+            handleFileChange={handleFileChange}
+            open={open}
+            setOpen={setOpen}
+          />
+        </div>
+
+        <PostContentSelect />
+
+        {/* <div className="w-full mb-8">
+        <MenuBar editor={editor} />
+        <RichTextEditor editor={editor} />
+      </div> */}
+
+        <PrimarySubmitButton className="mt-4" ref={buttonRef} text="Publish" />
+      </form>
+    </Form>
+  );
+};
+
+export default PostForm;
