2a9732f1d65b721f3390a8c6e977bac933f54273
Emilio Rivera <emiliorivera747@gmail.com>
2025-11-01 13:10:10 -0700

    Added usePostComposer and it's corressponding state

--- features/blog-composer/hooks/usePostComposer.ts
+++ features/blog-composer/hooks/usePostComposer.ts
@@ -1,9 +1,53 @@
+import { useState } from "react";
+
+// Hooks
+import useSubmitPost from "@/features/blog-composer/hooks/useSubmitPost";
+
+// Types
+import { ContentBlock } from "@/features/blogs/types/post";
 
 /**
  * The hook will handle the the composer state
  */
 const usePostComposer = () => {
-  return {};
+    
+  /**
+   * The current block being highlighted
+   */
+  const [currentBlockId, setCurrentBlockId] = useState<number | string>(0);
+
+  /**
+   * The content blocks
+   */
+  const [blocks, setBlocks] = useState<ContentBlock[]>([]);
+
+  /**
+   * Submits the post
+   */
+  const { onSubmit } = useSubmitPost({ contentBlocks: [] });
+
+  const handleEnterEditMode = (id: string) => {
+    setCurrentBlockId(id);
+  };
+
+  const addBlock = (block: ContentBlock) => {
+    setBlocks((prevBlocks) => [...prevBlocks, block]);
+  };
+
+  const handleBlockId = (id: string) => {
+    setCurrentBlockId(id);
+  };
+
+  return {
+    currentBlockId,
+    setCurrentBlockId,
+    blocks,
+    setBlocks,
+    onSubmit,
+    handleEnterEditMode,
+    addBlock,
+    handleBlockId,
+  };
 };
 
 export default usePostComposer;
