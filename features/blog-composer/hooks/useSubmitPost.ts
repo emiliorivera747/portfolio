d665016427f72e4cb7eb0ca04c89355249a15c7a
Emilio Rivera <emiliorivera747@gmail.com>
2025-11-05 12:05:16 -0800

    useSubmit hook handles multiple blocks

--- features/blog-composer/hooks/useSubmitPost.ts
+++ features/blog-composer/hooks/useSubmitPost.ts
@@ -7,6 +7,9 @@ import { ContentBlock } from "@/features/blogs/types/post";
 // Hooks
 import useCreatePost from "@/features/blog-composer/hooks/useCreatePost";
 
+// Context
+import { useComposerContext } from "@/features/blog-composer/context/ComposerContext";
+
 /**
  *
  * The hook is responsible for submitting the post
@@ -17,17 +20,26 @@ const useSubmitPost = ({
 }: {
   contentBlocks: ContentBlock[] | [];
 }) => {
-  
+  const { blocks } = useComposerContext();
+
   const { mutatePost, isPendingPost } = useCreatePost();
 
   const onSubmit = (data: FormSchema): void => {
-    const post: PostWithRelations = {
+    const postData: PostWithRelations = {
       title: data.title,
-      content_blocks: contentBlocks,
       description: data.description,
+      content_blocks: blocks.map(
+        ({ content_order, content_data, content_type, media }, index) => ({
+          id: `block-${index}`, // Generate a unique id for each block
+          content_order,
+          content_type, 
+          content_data, 
+          media: media ?? undefined,
+        })
+      ),
     };
 
-    mutatePost(post);
+    mutatePost(postData);
   };
 
   return { onSubmit };
