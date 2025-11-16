d747260f0456793e3aa38ed7f150f01d989861c2
Emilio Rivera <emiliorivera747@gmail.com>
2025-11-15 18:49:39 -0800

    Create upload-media service

--- services/media-storage/cloudinary-service.ts
+++ services/media-storage/cloudinary-service.ts
@@ -14,6 +14,7 @@ type UploadParams = {
   fileBuffer: Buffer;
   fileType: string;
   alt: string;
+  fileHash: string;
 };
 
 /**
---
+++ services/media-storage/upload-media.ts
@@ -0,0 +1,70 @@
+import { MediaUploadResult } from "@/types/mediaStorage";
+import { uploadToCloudinary } from "@/services/media-storage/cloudinary-service";
+import { PrismaClient } from "@prisma/client";
+import * as crypto from "crypto";
+
+const prisma = new PrismaClient();
+
+type UploadParams = {
+  fileBuffer: Buffer;
+  fileType: string;
+  alt: string;
+};
+
+export async function uploadMedia(
+  params: UploadParams
+): Promise<MediaUploadResult> {
+  const { fileBuffer, fileType, alt } = params;
+
+  // Calculates the file hash
+  const fileHash = crypto.createHash("sha256").update(fileBuffer).digest("hex");
+
+  // Check for existing Media by Hash
+  const existingMedia = await prisma.media.findUnique({
+    where: {
+      file_hash: fileHash,
+    },
+  });
+
+  if (existingMedia) {
+    console.log(
+      `Duplicate file detected for hash: ${fileHash}. Returning existing record.`
+    );
+
+    return {
+      url: existingMedia.url,
+      alt: existingMedia.alt || alt,
+      media_type: existingMedia.media_type as "image" | "video",
+      providerAssetId: existingMedia.provider_asset_id || "N/A",
+      storageProvider: existingMedia.storage_provider as "CLOUDINARY" | "S3",
+    };
+  }
+
+  // Not Duplicate
+  const provider = process.env.NEXT_PUBLIC_STORAGE_PROVIDER;
+  let uploadResult: MediaUploadResult;
+
+  if (provider === "CLOUDINARY") {
+    uploadResult = await uploadToCloudinary({ ...params, fileHash });
+  } else {
+    uploadResult = await uploadToCloudinary({ ...params, fileHash });
+  }
+
+  const newMedia = await prisma.media.create({
+    data: {
+      url: uploadResult.url,
+      media_type: uploadResult.media_type,
+      description: uploadResult.alt,
+      alt: uploadResult.alt,
+      provider_asset_id: uploadResult.providerAssetId,
+      storage_provider: uploadResult.storageProvider,
+      file_hash: fileHash,
+    },
+  });
+
+  return {
+    ...uploadResult,
+    url: newMedia.url,
+    alt: newMedia.alt || alt,
+  };
+}
