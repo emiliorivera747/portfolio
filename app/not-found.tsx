ff0d6d113790497cce541870cef08a81206ca4d4
Emilio Rivera <emiliorivera747@gmail.com>
2025-11-28 15:05:50 -0800

    Added 404 page not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen w-screen gap-2">
+      <h1 className="text-3xl font-bold">404 Page Not Found</h1>
+      <p className="text-primary-600 font-light">Sorry, we couldn&apos;t find this page.</p>
+    </div>
+  );
+}
