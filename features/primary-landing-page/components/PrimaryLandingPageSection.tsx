e3748c507ed72c2dabc4a1bf808674754da067d8
Emilio Rivera <emiliorivera747@gmail.com>
2026-04-30 13:37:03 -0700

    Updates background color for initial load home page

--- features/primary-landing-page/components/PrimaryLandingPageSection.tsx
+++ features/primary-landing-page/components/PrimaryLandingPageSection.tsx
@@ -40,7 +40,7 @@ const PrimaryLandingPageSection: React.FC<{ videoUrl?: string }> = ({
   useVideoIntersectionObserver(videoRef);
 
   return (
-    <section className="relative h-screen w-screen">
+    <section className="relative h-screen w-screen bg-black">
       <motion.div
         style={{ zIndex: 50 }}
         {...{
